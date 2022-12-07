fc8523c91fd923b1bf5372fee0cd7bb881ff137f
Kauã <kakadzn@hotmail.com>
2022-12-07 18:15:31 -0300

    if-elseif-else

--- JavaScript e TypeScript do básico ao avançado 2022/ifElseifElse/js/index.js
+++ JavaScript e TypeScript do básico ao avançado 2022/ifElseifElse/js/index.js
@@ -1,8 +1,24 @@
+/*
+Entre 0 - 11 -> Bom dia
+Entre 12 - 17 -> Boa tarde
+Entre 18 - 23 -> Boa noite
+*/
+
+// If pode ser usado sozinha
+// Sempre que for usar um else tem que ter um if antes
+// Pode ter vários else if na checagem
+// Só pode ter um else na checagem
+// Podemos usar condições sem else if, utilizando apenas if e else
+
 const hora = 10;
 
 // if -> se caso aconteça algo faca isso
-if (hora < 12) { 
+if (hora >= 0 && hora <= 11) { 
     console.log('Bom dia!');
-} else {
-    console.log('Boa tarde!')
+} else if (hora >= 12 && hora <= 17) { // senão -> caso não aconteça o if
+    console.log('Boa tarde!');
+} else if (hora >= 18 && hora <= 23){
+    console.log('Boa noite!');
+} else { // então -> caso não aconteça nenhuma das outras opções
+    console.log('Olá');
 }
