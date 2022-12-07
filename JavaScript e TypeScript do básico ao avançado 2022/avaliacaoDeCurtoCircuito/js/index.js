a90e9efab4684be32bcedfd937906423fd19cfdc
olakauã <kakadzn@hotmail.com>
2022-12-07 10:02:54 -0300

    avaliação-curto-circuito

---
+++ JavaScript e TypeScript do básico ao avançado 2022/avaliacaoDeCurtoCircuito/js/index.js
@@ -0,0 +1,11 @@
+/*
+&& -> false && true -> false -> Retorna o primeiro false
+|| -> true && false -> true -> Retorna o primeiro valor verdadeiro
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
