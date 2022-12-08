ff9516e301b71ed0960b6653638f5a7b08b31e7a
olakauã <kakadzn@hotmail.com>
2022-12-08 17:39:08 -0300

    switchCase

---
+++ JavaScript e TypeScript do básico ao avançado 2022/switchCase/js/index.js
@@ -0,0 +1,27 @@
+const data = new Date("2003-11-26 00:00:00");
+let diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diaSemana) {
+  case 0:
+    diaSemanaTexto = "Domingo";
+    break;
+  case 0:
+    diaSemanaTexto = "Domingo";
+    break;
+  case 0:
+    diaSemanaTexto = "Domingo";
+    break;
+  case 0:
+    diaSemanaTexto = "Segunda";
+    break;
+  case 0:
+    diaSemanaTexto = "Terça";
+    break;
+  case 0:
+    diaSemanaTexto = "Quarta";
+    break;
+  case 0:
+    diaSemanaTexto = "Quinta";
+    break;
+}
