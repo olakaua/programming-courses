9084b9f90101b1fc2f5b4680bd8ee4a9b579f105
olakaua <kakadzn@hotmail.com>
2022-12-27 14:13:43 -0300

    numbers

--- JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/02-TIPOS-DE-DADO-E-OPERADORES-LÓGICOS/Number/1-number/scripts.js
+++ JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/02-TIPOS-DE-DADO-E-OPERADORES-LÓGICOS/Number/1-number/scripts.js
@@ -2,8 +2,10 @@
 console.log(5);
 console.log(12.7);
 console.log(-123);
+console.log(-12.34);
 
 //Imprime o tipo do dado
 console.log(typeof 5);
 console.log(typeof 12.7);
 console.log(typeof -123);
+console.log(typeof -12.34);
