35b8802f80a6d431f9bd7b127231c9a0b83c36f4
olakaua <kakadzn@hotmail.com>
2022-12-27 11:03:29 -0300

    tipos de dado number

---
+++ JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/02-TIPOS-DE-DADO-E-OPERADORES-LÓGICOS/1-number/scripts.js
@@ -0,0 +1,9 @@
+// Imprime somente os valores
+console.log(5);
+console.log(12.7);
+console.log(-123);
+
+//Imprime o tipo do dado
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log(typeof -123);
