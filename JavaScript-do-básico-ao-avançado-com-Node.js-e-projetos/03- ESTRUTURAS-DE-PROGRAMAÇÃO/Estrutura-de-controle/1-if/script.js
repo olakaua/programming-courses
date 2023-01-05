40305026bf1a288f46dcb88aecb16972e9858db4
olakaua <kakadzn@hotmail.com>
2023-01-05 14:45:41 -0300

    estrutura-de-controle

---
+++ JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/03- ESTRUTURAS-DE-PROGRAMAÇÃO/Estrutura-de-controle/1-if/script.js
@@ -0,0 +1,21 @@
+let idade = 19;
+
+if(idade == 19) (
+    console.log('A idade é = 19')
+)
+
+if(idade > 25) {
+    console.log('A idade ẽ maior que 25')
+}
+
+let nome = 'Kauã'
+
+if(nome == 'Kauã' && idade > 10) {
+    console.log('Liberado');
+}
+
+let passaporte = true;
+
+if(nome == 'Kauã' && idade > 10 || passaporte == true) {
+    console.log('Liberado')
+}
---
+++ JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/03- ESTRUTURAS-DE-PROGRAMAÇÃO/Estrutura-de-controle/3-else-if/script.js
@@ -0,0 +1,10 @@
+let nome = 'Kauã';
+let idade = 19;
+
+if(nome != undefined && nome == 'Joaquim') {
+    console.log('Nome está definido')
+} else if(nome == 'Kauã' && nome.length > 3 && idade == 19) {
+    console.log('O nome é Kauã')
+} else {
+    console.log('Não é o Kauã!')
+}
