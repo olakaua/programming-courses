572dc44fbb41334199de7437039d0b090fdb178e
olakaua <kakadzn@hotmail.com>
2023-01-08 05:13:58 -0300

    switch/case

---
+++ JavaScript-do-básico-ao-avançado-com-Node.js-e-projetos/03- ESTRUTURAS-DE-PROGRAMAÇÃO/Estrutura-de-controle/4-switch/script.js
@@ -0,0 +1,13 @@
+ let nome = 'Kauã';
+
+ switch(nome){
+    case 'Kauã':
+        console.log('O nome é Kauã');
+        break;
+    case 'João':
+        console.log('O nome é João');
+        break;
+    default:
+        console.log('O nome não foi encontrado');
+        break;
+ }
